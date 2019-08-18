49f150b411e9c20e112899854f9d4bdba6ec4c5a
Paul <pwolanski43@gmail.com>
2019-08-17 21:14:58 -0400

    Finished HW draft

--- 03-Python/python-challenge/main.py
+++ 03-Python/python-challenge/main.py
@@ -1,14 +1,45 @@
 import os
 import csv
+total_months = 0
+net_total = 0
+average_change = 0
+losses_total = []
+profit_total = []
+lister = []
 
-budget_data = os.path.join('Resource', 'budget_data.csv')
-print(budget_data)
+budget_data = os.path.join('Resources', 'budget_data.csv')
+output_path = os.path.join("Resources", "Output.csv")
 
 with open (budget_data, newline='') as budget_data2:
     csv_reader = csv.reader(budget_data2, delimiter=',')
-    print(csv_reader)
+    if csv.Sniffer().has_header:
+        next(csv_reader)
 
-    csv_header = next(csv_reader)
-    print(f"CSV Header: {csv_header}")
-    for rows in csv_reader:
-        print(rows)
+    for row in csv_reader:
+       total_months += 1
+       net_total += int(row[1])
+       if int(row[1]) < 0:
+           losses_total.append(int(row[1]))
+       if int(row[1]) > 0:
+           profit_total.append(int(row[1]))
+
+    average_change = net_total/total_months
+
+    print(f'')
+    print(f"Financial Analysis")
+    print(f'--------------------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Net Total: ${net_total}')
+    print(f'Average Change: ${round(average_change,2)}')
+    print('Greatest Increase in Profits: $', max(profit_total))
+    print(f'Greatest Decrease in Profits: $', min(losses_total))
+
+budget_data2.close()
+mini_loss = min(losses_total)
+max_prof = max(profit_total)
+lister = [str(total_months), str(net_total), str(round(average_change, 2)), str(max_prof), str(mini_loss)]
+
+with open(output_path, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter=',')
+    csv_writer.writerow(['Total Months','Net Total','Average Change', 'Greatest Profit Increase','Greatest Profit Decrease'])
+    csv_writer.writerows([lister])
